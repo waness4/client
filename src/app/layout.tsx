1bedd492a3c195e828456e397bd551faf05675bf
wanessa_laurindo <wanessawlrodrigues@gmail.com>
2024-11-29 08:50:12 -0300

    feat: inicialização + navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Dashboard from "./dashboard/page";
+import DashboardWrapper from "./dashboardWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
